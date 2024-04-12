f0332f5959b6160d6a087e452c266805e35381cb
camry <camry.chen@foxmail.com>
2024-04-12 18:09:33 +0800

    UPDATE cmd/protoc-gen-go/internal_gengo/main.go

--- cmd/protoc-gen-go/internal_gengo/main.go
+++ cmd/protoc-gen-go/internal_gengo/main.go
@@ -738,7 +738,7 @@ func fieldGORMTagValue(field *protogen.Field) string {
 }
 
 func fieldRedisTagValue(field *protogen.Field) string {
-    return string(field.Desc.Name()) + ",omitempty"
+    return string(field.Desc.Name())
 }
 
 func genExtensions(g *protogen.GeneratedFile, f *fileInfo) {
